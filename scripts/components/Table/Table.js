11c577fc46cd32783ef336397abb06f1228e25ba
Alexey Makarov <alexeimakarow@gmail.com>
2019-03-02 15:01:45 +0100

    table sorter added

--- scripts/components/Table/Table.js
+++ scripts/components/Table/Table.js
@@ -1,36 +1,92 @@
 import Component from '../Component/Component.js';
 
 export default class Table extends Component {
-  constructor({ element, data }) {
-    super({ element });
+    constructor({element, data}) {
+        super({element});
 
-    this._render(data);
+        this._render(data);
 
-    this._el.addEventListener('click', e => this._onRowClick(e));
-  }
-
-  _onRowClick(e) {
-    const target = e.target.closest('tr');
-    if (!target) return;
-
-    const id = target.dataset.id;
-    if (id) {
-      let customEvent = new CustomEvent('rowClick', {
-        detail: id,
-      });
-      this._el.dispatchEvent(customEvent)
+        this._el.addEventListener('click', e => this._onRowClick(e));
+        this._el.addEventListener('click', e => this._sort(e));
     }
-  }
 
-  _render(data) {
-    this._el.innerHTML = `
+    _onRowClick(e) {
+        const target = e.target.closest('tr');
+        if (!target) return;
+
+        const id = target.dataset.id;
+        if (id) {
+            let customEvent = new CustomEvent('rowClick', {
+                detail: id,
+            });
+            this._el.dispatchEvent(customEvent)
+        }
+    }
+
+    _sort(e) {
+        if (e.target.nodeName !== 'TH') return;
+
+        let target = e.target;
+        let columnDataType = target.dataset.type;
+        let tbody = this._el.querySelector('tbody');
+        let thead = this._el.querySelector('thead');
+        let theadRow = thead.rows[0];
+        let tbodyRows = this._el.querySelector('tbody').rows;
+        let cellsToSortArr = [];
+
+        for (let th of theadRow.cells) {
+            th.classList.remove('active');
+        }
+
+        target.classList.add('active');
+
+        for (let tr of tbodyRows) {
+            cellsToSortArr.push(tr.cells[target.cellIndex].innerHTML)
+        }
+
+        if (columnDataType === 'string') {
+            cellsToSortArr.sort(function (a, b) {
+                return ('' + a).localeCompare(b);
+            });
+        } else if (columnDataType === 'number') {
+            cellsToSortArr.sort(function (a, b) {
+                return a - b;
+            });
+        }
+
+        let sort = function() {};
+
+        if (target.classList.contains('sorted')) {
+            sort = function(row) {
+                tbody.prepend(row);
+            };
+            target.classList.toggle('sorted');
+        } else {
+            sort = function(row) {
+                tbody.append(row);
+            };
+            target.classList.toggle('sorted');
+        }
+
+        for (let i = 0; i < cellsToSortArr.length; i++) {
+            for (let tr of tbodyRows) {
+                if (tr.cells[target.cellIndex].innerHTML === cellsToSortArr[i]) {
+                    sort(tr);
+                }
+            }
+        }
+
+    }
+
+    _render(data) {
+        this._el.innerHTML = `
     <table class="data-table highlight"> 
         <thead>
         <tr>
-            <th>Name</th>
-            <th>Symbol</th>
-            <th>Rank</th>
-            <th>Price</th>
+            <th data-type="string">Name</th>
+            <th data-type="string">Symbol</th>
+            <th data-type="number">Rank</th>
+            <th data-type="number">Price</th>
         </tr>
         </thead>
 
@@ -47,5 +103,5 @@ export default class Table extends Component {
     </table>
 
     `
-  }
+    }
 }
